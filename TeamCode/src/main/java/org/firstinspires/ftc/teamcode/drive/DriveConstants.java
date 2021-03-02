0363a638e63c2a0e7cd3695553cf685ac6561626
Andrei Stan <standrei2003@yahoo.com>
2021-03-02 19:58:24 +0200

    TeleOP-ul arata mai binisor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -55,9 +55,13 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0148;
+    /*public static double kV = 0.0148;
     public static double kA = 0.002;
-    public static double kStatic = 0.0552;
+    public static double kStatic = 0.0552;*/
+
+    public static double kV = 0;
+    public static double kA = 0;
+    public static double kStatic = 0;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
